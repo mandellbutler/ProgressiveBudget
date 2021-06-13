fa52d8089aa6338cc61bf3e143a46c0b1019d5f9
Mandell Butler <mandell.butler@outlook.com>
2021-06-13 07:34:40 -0400

    Updated service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,13 +47,13 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   // non GET requests are not cached and requests to other origins are not cached
-  // if (
-  //   event.request.method !== "GET" ||
-  //   !event.request.url.startsWith(self.location.origin)
-  // ) {
-  //   event.respondWith(fetch(event.request));
-  //   return;
-  // }
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
   // handle runtime GET requests for data from /api routes
   if (event.request.url.includes("/api/transaction")) {
